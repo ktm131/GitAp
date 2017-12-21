0ec4675c9cea59f3861e69982fb9cc3c6c035640
Adam Bubula <bubulaadam@op.pl>
2017-12-21 14:39:55 +0100

    Dodaj pliki projektów.

---
+++ GitAp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitAp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
